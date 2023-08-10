11b6c85269cecabe7d36ff13343485f0f2157a04
Liran Cohen <c.liran.c@gmail.com>
2023-08-09 21:44:48 -0400

    consolidated filter construction, added tests, updated comments

--- src/handlers/records-read.ts
+++ src/handlers/records-read.ts
@@ -10,7 +10,6 @@ import { messageReplyFromError } from '../core/message-reply.js';
 import { RecordsRead } from '../interfaces/records-read.js';
 import { RecordsWrite } from '../interfaces/records-write.js';
 import { DataStream, Encoder } from '../index.js';
-import { DwnError, DwnErrorCode } from '../index.js';
 import { DwnInterfaceName, DwnMethodName } from '../core/message.js';
 
 export class RecordsReadHandler implements MethodHandler {
